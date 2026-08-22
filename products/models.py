4103f9626d63178ff6b06beb2507d24110605beb
veltorex <radmehr.devops@gmail.com>
2026-08-22 13:26:55 +0330

    add product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.text import slugify
+import PIL
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
