de4196870c0e9c667f647b7b212ad52a512e2efa
veltorex <radmehr.devops@gmail.com>
2026-08-22 13:37:38 +0330

    add price field to product model

--- products/models.py
+++ products/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField()
+    price = models.DecimalField(max_digits=12, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
