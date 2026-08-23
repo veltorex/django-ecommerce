e4fbf678d255f5db0cb729ee55345c59998f9828
veltorex <radmehr.devops@gmail.com>
2026-08-23 21:12:32 +0330

    register and customize Product admin form

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product, ProductImage
 
-# Register your models here.
+
+# Customize product admin form
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ["title", "description", "price", "slug"]
+    readonly_fields = ("slug",)
+
+    list_display = ("title", "price", "created_at")
+    search_fields = ("title", "description")
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)
