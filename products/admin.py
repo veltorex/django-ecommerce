195969b69f773a91dd8ddf2916c44b488fb23073
veltorex <radmehr.devops@gmail.com>
2026-08-23 21:19:33 +0330

    add product image inline form to product admin form

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from .models import Product, ProductImage
 
 
+# Add product image form to product form
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+    fields = ("image",)
+    max_num = 10
+    min_num = 1
+
+
 # Customize product admin form
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -12,3 +21,5 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ("title", "description")
     list_filter = ("created_at",)
     ordering = ("-created_at",)
+    
+    inlines = [ProductImageInline]
