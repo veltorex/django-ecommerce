7aa01a69e82402b1af34f6057992a5a3542d27e9
veltorex <radmehr.devops@gmail.com>
2026-08-24 13:42:38 +0330

    add url for product creation form

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import product_list, product_detail
+from .views import product_list, product_detail, product_create
 
 urlpatterns = [
     path("", product_list, name="product-list"),
     path("<int:pk>/", product_detail, name="product-detail"),
+    path("create/", product_create, name="product-create")
 ]
