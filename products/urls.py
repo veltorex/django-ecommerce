304841440dd4845e6e79f50e65d6830e9a22a855
veltorex <radmehr.devops@gmail.com>
2026-08-26 12:34:35 +0330

    add url for product delete view

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import product_list, product_detail, product_create
+from .views import product_list, product_detail, product_create, prodcut_delete
 
 urlpatterns = [
     path("", product_list, name="product-list"),
     path("<int:pk>/", product_detail, name="product-detail"),
-    path("create/", product_create, name="product-create")
+    path("create/", product_create, name="product-create"),
+    path("delete/", prodcut_delete, name="product-delete"),
 ]
