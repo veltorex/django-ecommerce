517d9a6c603db3e949148e61adbe72224beccb0c
veltorex <radmehr.devops@gmail.com>
2026-08-29 19:42:14 +0330

    add category relation to product

--- products/models.py
+++ products/models.py
@@ -12,6 +12,11 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=12, decimal_places=2)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products",
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
