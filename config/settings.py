a9676cec7b4e945117cd887c528c7d3284357909
veltorex <radmehr.devops@gmail.com>
2026-08-31 21:58:50 +0330

    add and configure custom user model

--- config/settings.py
+++ config/settings.py
@@ -104,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "users.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
--- users/models.py
+++ users/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        # Send error when either one of the fields is empty or both are empty
+        if not email:
+            raise ValueError("User must have an email!")
+        
+        if not password:
+            raise ValueError("User must have a password!")
+        
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email=email,
+            password=password,
+        )
+        
+        # Add admin attributes to user
+        user.is_superuser = True
+        user.is_staff = True
+        
+        user.save(using=self._db)
+        
+        return user
+        
+        
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name="Email address",
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    # Connect UserManager with User
+    objects = UserManager()
+    
+    def __str__(self):
+        return self.email
+    
