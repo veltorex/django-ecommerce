430ba301fc0d3dbb0861933a4eca8dbb01e3b0fc
veltorex <radmehr.devops@gmail.com>
2026-09-01 20:41:32 +0330

    add register view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserCreationForm
 
 # Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("product-list")
+        
+    else:
+        form = UserCreationForm()
+        
+    return render(request, "registration/register.html", {"form": form})
+        
