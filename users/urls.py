6a928011217c9ad92c975cdf9fb0a11d29d7e622
veltorex <radmehr.devops@gmail.com>
2026-09-01 20:42:57 +0330

    add URL for register view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from .views import register
 
 app_name = "users"
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
+    path("register/", register, name="register")
 ]
