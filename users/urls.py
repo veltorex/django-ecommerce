e03daa9b78f7d9f83864823d72e4af43271bfc75
veltorex <radmehr.devops@gmail.com>
2026-09-01 21:24:46 +0330

    add URL for profile view

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import register
+from .views import register, profile
 
 app_name = "users"
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
-    path("register/", register, name="register")
+    path("register/", register, name="register"),
+    path("profile/", profile, name="profile"),
 ]
