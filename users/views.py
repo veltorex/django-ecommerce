277bce1f745cc2c9eed8ec0c20d8b5853ee5fd95
veltorex <radmehr.devops@gmail.com>
2026-09-02 15:33:42 +0330

    add address creation view

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from .forms import UserCreationForm, ProfileForm
+from .forms import UserCreationForm, ProfileForm, AddressForm
 from .models import Profile
 
 # Create your views here.
@@ -49,3 +49,22 @@ def address_list(request):
     addresses = request.user.addresses.all()
     
     return render(request, "users/address_list.html", {"addresses": addresses})
+
+
+@login_required
+def add_address(request):
+    if request.method == "POST":
+        form = AddressForm(request.POST)
+        
+        if form.is_valid():
+            address = form.save(commit=False)
+            address.user = request.user
+            address.save()
+            return redirect("users:address-list")
+        
+    else:
+        form =  AddressForm()
+        
+    return render(request, "users/add_address.html", {"form": form})
+        
+    
