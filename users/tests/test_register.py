6ce74b0338d9b7608f3350f6f781c86107cbf6dd
Radmehr <rademhr.devops@gmail.com>
2026-09-17 16:04:02 +0330

    add profile creation test to registration

--- users/tests/test_register.py
+++ users/tests/test_register.py
@@ -2,7 +2,8 @@
 
 from django.test import TestCase
 from django.urls import reverse
-from ..models import User
+from ..models import User, Profile
+
 
 class TestRegister(TestCase):
     def setUp(self):
@@ -23,3 +24,21 @@ class TestRegister(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse("products:product-list"))
         self.assertTrue(User.objects.filter(email=self.email).exists())
+
+
+    # Test if user profile is created
+    def test_user_profile_created(self):
+        self.client.post(
+            reverse("register"),
+            {
+                "email": self.email,
+                "password1": self.password,
+                "password2": self.password,
+            },
+        )
+
+        user = User.objects.get(email=self.email)
+
+        self.assertTrue(
+            Profile.objects.filter(user=user).exists()
+        )
